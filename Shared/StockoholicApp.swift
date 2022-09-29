6ba1865bdd79b46ebde897207945f72dc8ffc0c2
DaDevMate <67144384+dev365b@users.noreply.github.com>
2022-09-30 00:13:20 +0800

    Initial Commit

---
+++ Shared/StockoholicApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockoholicApp.swift
+//  Shared
+//
+//  Created by Nethan on 30/9/22.
+//
+
+import SwiftUI
+
+@main
+struct StockoholicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
