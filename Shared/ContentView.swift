56b66c04401ef316841263494837e8923b82a589
Angus chua <angusc8774@gmail.com>
2022-11-03 14:38:19 +0800

    layout

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,86 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(red: 0.03111111111, green: 0.14666666666, blue: 0.20444444444)
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("[ USERNAME ]")
+                    .foregroundColor(Color(red: 0.86222222222, green: 0.93777777777, blue: 0.91111111111))
+                    .font(.largeTitle)
+                Circle()
+                    .frame(width: 150, height: 150)
+                    .padding(40)
+                    .foregroundColor(.gray)
+                HStack{
+                    Button {
+                        print("hello")
+                    } label: {
+                        VStack{
+                            Image(systemName: "chart.line.uptrend.xyaxis")
+                            Text("Stocks")
+                        }
+                        .frame(width: 100, height: 80)
+                        .padding(40)
+                        .font(.title3)
+                        .foregroundColor(Color(red: 0.86222222222, green: 0.93777777777, blue: 0.91111111111))
+                            .background(Color(red: 0.03111111111, green: 0.14666666666, blue: 0.20444444444))
+
+                        .border(Color(red: 0.86222222222, green: 0.93777777777, blue: 0.91111111111), width: 5)
+                        .cornerRadius(10)
+                    }
+                    Button {
+                        print("hello")
+                    } label: {
+                        VStack{
+                            Image(systemName: "person.3.sequence.fill")
+                            Text("Start-ups")
+                        }
+                            .frame(width: 100, height: 80)
+                            .padding(40)
+                            .font(.title3)
+                            .foregroundColor(Color(red: 0.86222222222, green: 0.93777777777, blue: 0.91111111111))
+                                .background(Color(red: 0.03111111111, green: 0.14666666666, blue: 0.20444444444))
+
+                            .border(Color(red: 0.86222222222, green: 0.93777777777, blue: 0.91111111111), width: 5)
+                            .cornerRadius(10)
+                    }
+                }
+                HStack{
+                    Button {
+                        print("hello")
+                    } label: {
+                        VStack {
+                            Image(systemName: "dollarsign.arrow.circlepath")
+                            Text("Portfolio")
+                        }
+                            .frame(width: 100, height: 80)
+                            .padding(40)
+                         .font(.title3)
+                            .foregroundColor(Color(red: 0.86222222222, green: 0.93777777777, blue: 0.91111111111))
+                                .background(Color(red: 0.03111111111, green: 0.14666666666, blue: 0.20444444444))
+                            .border(Color(red: 0.86222222222, green: 0.93777777777, blue: 0.91111111111), width: 5)
+                            .cornerRadius(10)
+                    }
+                    Button {
+                        print("hello")
+                    } label: {
+                        VStack{
+                            Image(systemName: "flame")
+                            Text("Your Porgess")
+                        }
+                            .frame(width: 100, height: 80)
+                            .padding(40)
+                            .font(.title3)
+                            .foregroundColor(Color(red: 0.86222222222, green: 0.93777777777, blue: 0.91111111111))
+                                .background(Color(red: 0.03111111111, green: 0.14666666666, blue: 0.20444444444))
+
+                            .border(Color(red: 0.86222222222, green: 0.93777777777, blue: 0.91111111111), width: 5)
+                            .cornerRadius(10)
+                    }
+                }
+            }
+        }
     }
 }
 
