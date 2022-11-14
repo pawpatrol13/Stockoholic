20e781ef73bbf53952e63261f8abceca9a78ba08
Daryl <daryllim3bro@gmail.com>
2022-11-14 13:24:04 +0800

    Fixed the stock view (pls format ur work with cmd-a + ctrl-i)

--- Shared/StockView.swift
+++ Shared/StockView.swift
@@ -25,26 +25,39 @@ struct StockView: View {
         "Apple", "Tesla", "Twitter", "Mircosoft", "Yahoo", "Bank of China", "Google", "Alphabet", "Amazon", "Meta Platforms", "Nvidia" , "Berkshire Hathaway Class B"
     ]
     var body: some View {
-        NavigationLink(destination: StockDataView()) {
-            ZStack {
-                Color("BackgroundColor")
-                    .edgesIgnoringSafeArea(.all)
-                VStack(alignment: .leading) {
-                    ZStack {
-                        Rectangle()
-                            .foregroundColor(Color(red: 0.76862745098, green: 0.83137254902, blue: 0.81176470588))
-                        HStack {
-                            Image(systemName: "magnifyingglass")
-                            TextField("Search ..", text: $searchText)
-                        }
-                        
-                        .foregroundColor(Color(red: 0.03111111111, green: 0.14666666666, blue: 0.20444444444))
-                        .padding(13)
+        ZStack{
+            Color("BackgroundColor")
+                .edgesIgnoringSafeArea(.all)
+            VStack(alignment: .leading){
+                VStack {
+                    Button {
+                        showingSheet2.toggle()
+                    } label: {
+                        Image(systemName:  "text.book.closed")
+                            .font(.title)
+                            .padding()
                     }
-                    .frame(height: 60)
-                    .cornerRadius(13)
-                    .padding()
-                    .offset(y:40)
+                    .sheet(isPresented: $showingSheet2) {
+                        SheetView2()
+                    }
+                }
+                
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(Color(red: 0.76862745098, green: 0.83137254902, blue: 0.81176470588))
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                        TextField("Search ..", text: $searchText)
+                    }
+                    
+                    .foregroundColor(Color(red: 0.03111111111, green: 0.14666666666, blue: 0.20444444444))
+                    .padding(13)
+                }
+                .frame(height: 40)
+                .cornerRadius(13)
+                .padding()
+                
+                NavigationLink(destination: StockDataView()) {
                     ZStack {
                         Color(red: 0.03111111111, green: 0.14666666666, blue: 0.20444444444)
                             .edgesIgnoringSafeArea(.all)
@@ -63,30 +76,13 @@ struct StockView: View {
                         
                     }
                 }
-                VStack {
-                    Button {
-                        showingSheet2.toggle()
-                    } label: {
-                        Image(systemName:  "text.book.closed")
-                            .font(.title)
-                    }
-                    .sheet(isPresented: $showingSheet2) {
-                        SheetView2()
-                }
-                    .position(x: 27, y: 15)
-                    
-                }
             }
         }
-        
     }
-    
-}
-
-struct StockView_Previews: PreviewProvider {
-    static var previews: some View {
-        StockView()
-            .preferredColorScheme(.dark)
+    struct StockView_Previews: PreviewProvider {
+        static var previews: some View {
+            StockView()
+                .preferredColorScheme(.dark)
+        }
     }
-    
 }
