acbfd7314d9a769a883097fc3a695ce418003139
Daryl <daryllim3bro@gmail.com>
2022-11-16 14:09:32 +0800

    Cosmetic Tabview changes

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,29 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection = 3
+    init(){
+        UITabBar.appearance().backgroundColor = UIColor.systemGray6
+    }
+    
     var body: some View {
-        TabView{
+        TabView(selection: $selection){
             StartupView()
                 .tabItem{
                     Label("Start-ups",systemImage: "person.3")
                 }
+                .tag(1)
             StockView()
                 .tabItem{
                     Label("Stocks",systemImage: "chart.xyaxis.line")
                 }
+                .tag(2)
             PortfoilioView()
                 .tabItem{
                     Label("Portfolio",systemImage: "list.bullet.rectangle.portrait")
                 }
+                .tag(3)
             ProgressView()
                 .tabItem{
                     Label("Progress",systemImage: "flame")
                 }
+                .tag(4)
             ShopView()
                 .tabItem{
                     Label("Shop",systemImage: "cart")
                 }
+                .tag(5)
         }
+        .tint(Color("ForegroundColor"))
     }
 }
 struct ContentView_Previews: PreviewProvider {
