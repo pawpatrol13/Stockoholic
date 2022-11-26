df5784062fe1618c0fb90e96539236ada53ae49e
Ng Aaron <aaronng07@yahoo.com>
2022-11-26 09:16:10 +0800

    App works (finally)

--- Shared/BuyAndSellView.swift
+++ Shared/BuyAndSellView.swift
@@ -143,8 +143,13 @@ struct BuyAndSellView: View {
                         Stepper(value: $sharesBuying, in: 1...1000000){}
                     }
                     
-                    Text("Cost: $\(stockManager.stocks[stockNum].pricePerStockArray[0] * sharesBuying)")
-                        .fontWeight(.bold)
+                    HStack {
+                        Text("Cost: $\(stockManager.stocks[stockNum].pricePerStockArray[0] * sharesBuying)")
+                            .padding()
+                        Text("Cash after transaction: $\(cash - (stockManager.stocks[stockNum].pricePerStockArray[0] * sharesBuying))")
+                            .padding()
+                    }
+                    
                     HStack {
                         Spacer()
                         Button("Purchase") {
@@ -187,14 +192,21 @@ struct BuyAndSellView: View {
                             }
                             Spacer()
                             
-                            Text("Worth: $\(stockManager.stocks[stockNum].pricePerStockArray[0]*sharesToSell)")
-                            
                             HStack {
                                 TextField("Selling:", value: $sharesToSell, formatter: NumberFormatter())
                                     .keyboardType(.decimalPad)
                                     .textFieldStyle(RoundedBorderTextFieldStyle())
                                 Spacer()
                                 Stepper(value: $sharesToSell, in: 1...100000) {}
+                            }
+                            
+                            HStack{
+                                Text("Worth: $\(stockManager.stocks[stockNum].pricePerStockArray[0]*sharesToSell)")
+                                    .padding()
+                                Text("Cash after transaction: $\(cash + (stockManager.stocks[stockNum].pricePerStockArray[0] * sharesToSell))")
+                                    .padding()
+                                Text("Shares after transaction: \(stockManager.stocks[stockNum].stocksOwned - sharesToSell)")
+                                    .padding()
                             }
                             Spacer()
                         }
